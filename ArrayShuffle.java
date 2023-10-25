d9d5381fdf893d35b46b23a6d81508d046ee5dd0
himanchal7913 <90519701+himanchal7913@users.noreply.github.com>
2023-10-25 11:06:41 +0530

    Create ArrayShuffle.java

---
+++ ArrayShuffle.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayShuffle {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        shuffleArray(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void shuffleArray(int[] array) {
+        int n = array.length;
+        Random random = new Random();
+
+        for (int i = n - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+
+            // Swap array[i] and array[j]
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+
+
+// Output  [6, 7, 5, 2, 1, 3, 4]
